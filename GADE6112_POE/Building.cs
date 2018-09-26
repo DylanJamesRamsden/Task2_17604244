c30f5d1ea77fcb7719c1ab71e0916ce40f66ff10
DylanJamesRamsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2018-09-26 10:52:01 +0200

    Intital Commit

---
+++ GADE6112_POE/Building.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_POE
+{
+    [Serializable]
+    abstract class Building
+    {
+        protected int xpos;
+
+        protected int ypos;
+
+        protected int health;
+
+        protected string faction;
+
+        protected string symbol;
+
+        //public Building() 
+
+        abstract public bool isDead();
+
+        abstract public string toString();
+    }
+}
